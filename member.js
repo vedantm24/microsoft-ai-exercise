199e814bc7cbfe19d314f5e9a6623784027b204e
VEDANT MEHROTRA <53151707+vedantm24@users.noreply.github.com>
2023-07-22 06:56:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["Javascript", "HTML", "CSS"],
+        calculateNumbers: function(var1, var2) {
+            return var1 + var2;
+        }
+    }
+    return member;
+}
